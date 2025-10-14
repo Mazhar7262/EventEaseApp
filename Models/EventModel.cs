dcfc14bcc00ad4f4effb3cfc456886cd2dbc067e
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-10-14 15:25:33 +0100

    Initial commit

---
+++ Models/EventModel.cs
@@ -0,0 +1,9 @@
+namespace EventEaseApp.Models
+{
+    public class EventModel
+    {
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+    }
+}
