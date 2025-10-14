5a9f9c102c184d33a621c9530c89c56ef0d90626
Mazhar7262 <muqeet7262@gmail.com>
2025-10-14 16:31:39 +0100

    Add compressed binary file for project assets

--- obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcfc14bcc00ad4f4effb3cfc456886cd2dbc067e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
